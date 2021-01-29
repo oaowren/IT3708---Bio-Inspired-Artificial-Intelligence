5765429d6be766edcf8b0012ce424e5d753e387f
Odd Andre Owren <oddandreowren@gmail.com>
2021-01-29 10:37:18 +0100

    formatting

--- project1/individual.py
+++ project1/individual.py
@@ -5,7 +5,9 @@ class Individual():
 
     def __init__(self, dna):
         self.dna = dna
-        self.fitness = GA.fitness_sine(self.dna, len(self.dna), tuple([0, 128]))
+        self.fitness = GA.fitness_sine(
+            self.dna, len(self.dna),
+            tuple([0, 128]))
         self.age = 0
 
     def generation(self):
