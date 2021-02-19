0dc8807cbd638dea99e2656424d7c4cfd1a5a161
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-02-19 13:39:11 +0100

    Add new rows for depot

--- project2/Utils.java
+++ project2/Utils.java
@@ -12,18 +12,17 @@ public class Utils {
     public static List<List<Integer>> readDataFile(String filename) {
         try {
             List<String> lines = Files.readAllLines(Paths.get(Utils.class.getClass().getResource(filename).toURI()));
-            return lines.stream().map(
-                line -> Arrays.asList(line.split("\s+"))
-                              .stream()
-                              .map(string -> Integer.parseInt(string))
-                            .collect(Collectors.toList())
-                    ).collect(Collectors.toList()
-                );
+            return lines
+                    .stream().map(line -> Arrays.asList(line.split("\s+")).stream()
+                            .map(string -> Integer.parseInt(string)).collect(Collectors.toList()))
+                    .collect(Collectors.toList());
         } catch (IOException | URISyntaxException error) {
             System.out.println(error.toString());
             return null;
         }
     }
+    
+
 
     public static void processDataSet(List<List<Integer>> dataSet) {
         int maxNumOfVehicles = dataSet.get(0).get(0);
@@ -32,10 +31,13 @@ public class Utils {
 
         List<Depot> depots = new ArrayList<>();
         for (int i = 1; i <= numberOfDepots; i++) {
+            int endIndex = dataSet.size() - 1 - numberOfDepots + i;
             int maxRouteDuration = dataSet.get(i).get(0);
             int maxVehicleLoad = dataSet.get(i).get(1);
-            depots.add(new Depot(i, maxNumOfVehicles, maxRouteDuration, maxVehicleLoad));
+            depots.add(new Depot(i, maxNumOfVehicles, maxRouteDuration, maxVehicleLoad, dataSet.get(endIndex).get(1), dataSet.get(endIndex).get(2)));
         }
+
+
     }
 }
 
