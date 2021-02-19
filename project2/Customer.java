acc8d53be7c16bd71b040a365ef363c654b9fa9a
Odd Andre Owren <oddandreowren@gmail.com>
2021-02-19 12:09:04 +0100

    create Customer and Depot

---
+++ project2/Customer.java
@@ -0,0 +1,12 @@
+public class Customer {
+    private int id;
+
+    public Customer(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+}
---
+++ project2/Depot.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Depot {
+
+    private int id;
+    private List<Vehicle> vehicles = new ArrayList<Vehicle>();
+    
+    public Depot(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+
+}
