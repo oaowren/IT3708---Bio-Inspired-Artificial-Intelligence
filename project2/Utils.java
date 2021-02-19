e11b9063caccedf118d14a0e2344a07886508654
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-02-19 12:48:18 +0100

    Fix preprocessing of dataset

--- project2/Utils.java
+++ project2/Utils.java
@@ -2,29 +2,39 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
 
-    public static List<List<String>> readDataFile(String filename) {
+    public static List<List<Integer>> readDataFile(String filename) {
         try {
             List<String> lines = Files.readAllLines(Paths.get(Utils.class.getClass().getResource(filename).toURI()));
-            return lines.stream().map(line -> Arrays.asList(line.split("\s+"))).collect(Collectors.toList());
+            return lines.stream().map(
+                line -> Arrays.asList(line.split("\s+"))
+                              .stream()
+                              .map(string -> Integer.parseInt(string))
+                            .collect(Collectors.toList())
+                    ).collect(Collectors.toList()
+                );
         } catch (IOException | URISyntaxException error) {
             System.out.println(error.toString());
             return null;
         }
     }
 
-    public static void processDataSet(List<List<String>> dataSet) {
-        int maxNumOfVehicles = Integer.parseInt(dataSet.get(0).get(0));
-        int totalNumOfCustomers = Integer.parseInt(dataSet.get(0).get(1));
-        int numberOfDepots = Integer.parseInt(dataSet.get(0).get(2));
+    public static void processDataSet(List<List<Integer>> dataSet) {
+        int maxNumOfVehicles = dataSet.get(0).get(0);
+        int totalNumOfCustomers = dataSet.get(0).get(1);
+        int numberOfDepots = dataSet.get(0).get(2);
 
+        List<Depot> depots = new ArrayList<>();
         for (int i = 1; i <= numberOfDepots; i++) {
-            
+            int maxRouteDuration = dataSet.get(i).get(0);
+            int maxVehicleLoad = dataSet.get(i).get(1);
+            depots.add(new Depot(i, maxNumOfVehicles, maximumRouteDuration, maxVehicleLoad));
         }
     }
 }
