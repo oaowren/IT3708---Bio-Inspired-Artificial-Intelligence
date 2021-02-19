db0476ca1f164708685aba492d059528e411daed
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-02-19 13:46:09 +0100

    Add read of Customers

--- project2/Utils.java
+++ project2/Utils.java
@@ -26,18 +26,22 @@ public class Utils {
 
     public static void processDataSet(List<List<Integer>> dataSet) {
         int maxNumOfVehicles = dataSet.get(0).get(0);
-        int totalNumOfCustomers = dataSet.get(0).get(1);
+        int numOfCustomers = dataSet.get(0).get(1);
         int numberOfDepots = dataSet.get(0).get(2);
+        int depotIndexStartEnd = dataSet.size() - 1 - numberOfDepots;
 
         List<Depot> depots = new ArrayList<>();
         for (int i = 1; i <= numberOfDepots; i++) {
-            int endIndex = dataSet.size() - 1 - numberOfDepots + i;
+            int endIndex = depotIndexStartEnd + i;
             int maxRouteDuration = dataSet.get(i).get(0);
             int maxVehicleLoad = dataSet.get(i).get(1);
             depots.add(new Depot(i, maxNumOfVehicles, maxRouteDuration, maxVehicleLoad, dataSet.get(endIndex).get(1), dataSet.get(endIndex).get(2)));
         }
 
-
+        List<Customer> customers = new ArrayList<>();
+        for (List<Integer> customerRow : dataSet.subList(numberOfDepots+1, depotIndexStartEnd)) {
+            customers.add(new Customer(customerRow.get(0), customerRow.get(1), customerRow.get(2), customerRow.get(3), customerRow.get(4)));
+        }
     }
 }
 
