4cc3c30d68490b3e943b0e75cc48b0c5c3998963
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-02-19 15:09:17 +0100

    Add pivotal parameters

--- project2/Parameters.java
+++ project2/Parameters.java
@@ -1,3 +1,11 @@
 public class Parameters {
     
+    static final int populationSize = 400;
+    static final int generationSpan = 3000;
+    static final int eliteSize = 4;
+    static final double crossoverProbability = 0.60;
+    static final double intraDepotMutationProbability = 0.20;
+    static final double interDepotMutationProbability = 0.25;
+    static final double interDepotMutationRate = 10;
+
 }
