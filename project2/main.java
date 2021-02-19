7e169e193d563024b7a189de9b4a355b7a4a5546
Odd Andre Owren <oddandreowren@gmail.com>
2021-02-19 15:53:44 +0100

    add TODO-list

--- project2/main.java
+++ project2/main.java
@@ -17,4 +17,24 @@ class Main{
         v1.visitCustomer(c2);
         DataSetIo.writeResults(depots, "testorama.txt");
     }
+
+    /*TODO:
+    - Individual (in population)-Class with list of depots
+    - Memoization
+    - Random inital population
+     - Initialize individuals from dataset
+     - Add customers to random routes as long as constraints are satisfied until all customers have a vehicle assigned
+    - Parent selection
+     - Total fitness (focus on fewer cars/routes, then shorter total distance)
+    - Crossover
+     - Between depots
+      - Crossover length x from depot n and m, remove customers accordingly from the other depot
+      - Insert where it can satisfy constraints
+    - Mutation (intra-depot and inter-depot)
+     - Intra-depot e.g. inverse-mutation (reverse subset of route)
+     - Inter-depot find example
+    - Survivorselection
+     - Most likely elitism
+    - Total Individual fitness
+     - alpha * number of routes + beta * sum of distances*/
 }
