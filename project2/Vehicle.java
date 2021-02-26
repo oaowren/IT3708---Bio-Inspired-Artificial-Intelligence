b7f8b4700c38ef751c43bf9e53de67fe3237f895
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-02-26 09:22:07 +0100

    Add hash and equals methods to Vehicle

--- project2/Vehicle.java
+++ project2/Vehicle.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Objects;
 import java.util.ArrayList;
 import DataClasses.Customer;
 
@@ -6,7 +7,7 @@ public class Vehicle {
     
     public final int id, maxLoad;
     private int load = 0;
-    private List<Customer> customers = new ArrayList<Customer>();
+    private List<Customer> route = new ArrayList<Customer>();
     private Depot depot;
 
     public Vehicle(int id, int maxLoad) {
@@ -23,16 +24,33 @@ public class Vehicle {
             throw new IllegalStateException("Too much load for current route");
         }
         this.load += customer.demand;
-        this.customers.add(customer);
+        this.route.add(customer);
     }
 
     public String getCustomerSequence() {
-        return this.customers.stream().map(c -> Integer.toString(c.id)).reduce("",
+        return this.route.stream().map(c -> Integer.toString(c.id)).reduce("",
                 (output, c) -> output + (output.matches("") ? "" : " ") + c);
     }
 
     public boolean isActive() {
-        return customers.size() > 0;
+        return route.size() > 0;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Vehicle)) {
+            return false;
+        }
+        Vehicle vehicle = (Vehicle) o;
+        return id == vehicle.id && maxLoad == vehicle.maxLoad && load == vehicle.load && Objects.equals(route, vehicle.route) && Objects.equals(depot, vehicle.depot);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, maxLoad, load, route, depot);
+    }
+
 
 }
