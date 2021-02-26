0a714d9356ffc00f0da47278e484b07ade75e67b
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-02-26 08:58:37 +0100

    add isactive in vehicle

--- project2/Vehicle.java
+++ project2/Vehicle.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import DataClasses.Customer;
 
 public class Vehicle {
     
@@ -8,27 +9,30 @@ public class Vehicle {
     private List<Customer> customers = new ArrayList<Customer>();
     private Depot depot;
 
-    public Vehicle(int id, int maxLoad){
+    public Vehicle(int id, int maxLoad) {
         this.id = id;
         this.maxLoad = maxLoad;
     }
 
-    public void setDepot(Depot depot){
+    public void setDepot(Depot depot) {
         this.depot = depot;
     }
 
-    public void visitCustomer(Customer customer){
-        if (this.load + customer.demand > this.maxLoad){
+    public void visitCustomer(Customer customer) {
+        if (this.load + customer.demand > this.maxLoad) {
             throw new IllegalStateException("Too much load for current route");
         }
         this.load += customer.demand;
         this.customers.add(customer);
     }
 
-    public String getCustomerSequence(){
-        return this.customers.stream()
-                            .map(c -> Integer.toString(c.id))
-                            .reduce("", (output, c) -> output + (output.matches("")? "":" ") + c);
+    public String getCustomerSequence() {
+        return this.customers.stream().map(c -> Integer.toString(c.id)).reduce("",
+                (output, c) -> output + (output.matches("") ? "" : " ") + c);
+    }
+
+    public boolean isActive() {
+        return customers.size() > 0;
     }
 
 }
