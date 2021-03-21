ab7b396940ad548c9447d76800cd33fa929cf63f
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-03-21 21:39:00 +0100

    Update results

--- project2/Parameters.java
+++ project2/Parameters.java
@@ -1,16 +1,16 @@
 public class Parameters {
     
-    static final String problem = "p09";
+    static final String problem = "p23";
     static final int populationSize = 100;
     static final int generationSpan = 5000;
     static final int eliteSize = 2;
     static final int tournamentSize = 2;
     static final int parentSelectionSize = 80;
     static final double tournamentProb = 0.9;
-    static final double crossoverProbability = 0.60;
-    static final double mutationProbability = 0.25;
+    static final double crossoverProbability = 0.70;
+    static final double mutationProbability = 0.3;
     static final double interDepotMutationRate = 10;
-    static final double swappableCustomerDistance = 0.2;
+    static final double swappableCustomerDistance = 0.4;
 
     static final double alpha = 0; // Discount factor for number of active vehicles
     static final double beta = 0.01; // Discount factor for total route length
