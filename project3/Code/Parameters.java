12b71e4eae0b7a4f458cb66f3f93983aa645ab60
Odd Andre Owren <oddandreowren@gmail.com>
2021-04-08 10:42:03 +0200

    ran some tests

--- project3/Code/Parameters.java
+++ project3/Code/Parameters.java
@@ -3,10 +3,10 @@ package Code;
 public class Parameters {
     
     public static final String filename = "118035"; // Use folder name in training_images
-    public static final int populationSize = 10;
+    public static final int populationSize = 26;
     public static final int generationSpan = 10;
     public static final int eliteSize = 2;
-    public static final int parentSelectionSize = 5;
+    public static final int parentSelectionSize = 10;
     public static final double tournamentProb = 0.9;
     public static final double crossoverProbability = 0.7;
     public static final double singleGeneCrossoverProb = 0.5;
--- project3/Code/Pixel.java
+++ project3/Code/Pixel.java
@@ -1,7 +1,6 @@
 package Code;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
